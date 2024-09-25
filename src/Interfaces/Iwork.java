4943fb986ad8f88a328a5595af29add5bea3664c
Bogdan <Baga13511@yandex.ru>
2024-09-25 14:16:21 +0300

    First Commit

---
+++ src/Interfaces/Iwork.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+public interface Iwork {
+    void add(Object key, Object value);
+    void delete(Object key);
+    void put(Object key, Object value);
+    void viewAll();
+    void viewKey(Object key);
+}
---
+++ src/workWithDB.java
@@ -0,0 +1,39 @@
+import Interfaces.Iwork;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class workWithDB implements Iwork {
+    ConcurrentHashMap<Object, Object> concurrentHashMap = new ConcurrentHashMap<>();
+    /*
+     * TO DO:
+     * Добавить возможность пункта со временем, чтобы в дальнейшем удалять ключи (ttl)
+     * */
+    @Override
+    public void add(Object key, Object value) {
+        this.concurrentHashMap.put(key, value);
+    }
+
+    @Override
+    public void delete(Object key) {
+        this.concurrentHashMap.remove(key);
+    }
+
+    /*
+    * TO DO:
+    * Если такого ключа нет в системе - добавить
+    * Изменять время для ключа ttl
+    * */
+    @Override
+    public void put(Object key, Object value) {
+        this.concurrentHashMap.put(key, value);
+    }
+
+    @Override
+    public void viewAll(){
+        System.out.println(concurrentHashMap);
+    }
+
+    @Override
+    public void viewKey(Object key) {
+        System.out.println(concurrentHashMap.get(key));
+    }
+}
