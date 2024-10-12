b8eb50109dc7b820c51607e14e2345cf37677fe1
Bogdan <Baga13511@yandex.ru>
2024-10-12 11:07:58 +0300

    update

--- interfaces/ITable.go
+++ interfaces/ITable.go
@@ -3,9 +3,14 @@ package interfaces
 import "time"
 
 type ITable interface {
-	add(key interface{}, value interface{}, dayTime time.Time) (bool, error)
+	add(key interface{}, value Value) (bool, error)
 	delete(key interface{}) (bool, error)
-	put(key interface{}, value interface{}, dayTime time.Time) (bool, error)
+	put(key interface{}, value Value) (bool, error)
 	get(key interface{}) (interface{}, error)
 	size() int
+}
+
+type Value struct {
+	value interface{}
+	ttl   time.Time
 }
