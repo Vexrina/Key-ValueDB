f3d339859f523e2712e969609d648e1d6ba82797
Bogdan <Baga13511@yandex.ru>
2024-10-12 11:05:49 +0300

    update

--- interfaces/IDataBase.go
+++ interfaces/IDataBase.go
@@ -1,4 +1,8 @@
 package interfaces
 
 type IDataBase interface {
+	add(keyDB interface{}, table ITable) (bool, error)
+	get(keyDB interface{}) (ITable, error)
+	remove(keyDB interface{}) (bool, error)
+	put(keyDB interface{}, table ITable) (bool, error)
 }
--- interfaces/ITable.go
+++ interfaces/ITable.go
@@ -3,9 +3,9 @@ package interfaces
 import "time"
 
 type ITable interface {
-	add(key interface{}, value interface{}, dayTime time.Time)
-	delete(key interface{})
-	put(key interface{}, value interface{}, dayTime time.Time)
-	get(key interface{}) interface{}
+	add(key interface{}, value interface{}, dayTime time.Time) (bool, error)
+	delete(key interface{}) (bool, error)
+	put(key interface{}, value interface{}, dayTime time.Time) (bool, error)
+	get(key interface{}) (interface{}, error)
 	size() int
 }
