6d70b01f8e780655882851dd68ebb13bdff734d4
Bogdan <Baga13511@yandex.ru>
2024-11-18 14:38:08 +0300

    Updates

--- testAPI/test.py
+++ testAPI/test.py
@@ -32,23 +32,42 @@ def table():
 
 
 def test_create_database(base_url, endpoint, database):
-    response = httpx.post(base_url + endpoint["db"], json={"db_name": database})
+    response = httpx.post(
+        base_url + endpoint["db"],
+        json={"db_name": database},
+    )
     assert response.status_code == 201, f"Unexpected status code: {response.status_code}"
 
 def test_create_table(base_url, endpoint, database, table):
-    response = httpx.post(base_url + endpoint["table"] + "?db_name="+database, json={"table_name": table})
+    response = httpx.post(
+        base_url + endpoint["table"] + "?db_name="+database,
+        json={"table_name": table},
+    )
     assert response.status_code == 201, f"Unexpected status code: {response.status_code}"
 
 def test_put_table(base_url, endpoint, database, table):
-    response = httpx.put(base_url + endpoint["table"] + "?db_name=" + database,
-                         json={"table_name": table, "new_table_name": "table_database_new2"})
+    response = httpx.put(
+        base_url + endpoint["table"] + "?db_name=" + database,
+        json={
+            "table_name": table,
+            "new_table_name": "table_database_new2",
+        },
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
-    response = httpx.put(base_url + endpoint["table"] + "?db_name=" + database,
-                         json={"table_name": "table_database_new2", "new_table_name": table})
+    response = httpx.put(
+        base_url + endpoint["table"] + "?db_name=" + database,
+        json={
+            "table_name": "table_database_new2",
+            "new_table_name": table,
+        },
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
 
 def test_get_table(base_url, endpoint, database, table):
-    response = requests.get(base_url + endpoint["table"] + "?db_name=" + database, json={"table_name": table})
+    response = requests.get(
+        base_url + endpoint["table"] + "?db_name=" + database,
+        json={"table_name": table},
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
 
 def test_create_key(base_url, endpoint, database, table):
@@ -59,12 +78,18 @@ def test_create_key(base_url, endpoint, database, table):
             "Ttl": "2025-01-01T15:30:30Z"
         }
     }
-    response = httpx.post(base_url + endpoint["key"] + "?db_name=" + database+"&table_name="+table, json=json)
+    response = httpx.post(
+        base_url + endpoint["key"] + "?db_name=" + database+"&table_name="+table,
+        json=json,
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
 
 
 def test_get_key(base_url, endpoint, database, table):
-    response = requests.get(base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table, json={"key_name": "1"})
+    response = requests.get(
+        base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table,
+        json={"key_name": "1"},
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     assert response.json() == {
             "Val": "table_database",
@@ -79,10 +104,15 @@ def test_put_key(base_url, endpoint, database, table):
             "Ttl": "2025-01-01T15:30:30Z"
         }
     }
-    response = httpx.put(base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table, json=json)
+    response = httpx.put(
+        base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table,
+        json=json,
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
-    response = requests.get(base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table,
-                            json={"key_name": "1"})
+    response = requests.get(
+        base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table,
+        json={"key_name": "1"},
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
     assert response.json() == {
         "Val": "new_table_database",
@@ -90,14 +120,22 @@ def test_put_key(base_url, endpoint, database, table):
     }
 
 def test_delete_key(base_url, endpoint, database, table):
-    response = requests.delete(base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table, json={"key_name": "1"})
+    response = requests.delete(
+        base_url + endpoint["key"] + "?db_name=" + database + "&table_name=" + table,
+        json={"key_name": "1"},
+    )
     assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
 
 def test_delete_table(base_url, endpoint, database, table):
-    response = requests.delete(base_url + endpoint["table"] + "?db_name=" + database,
-                               json={"table_name":table})
+    response = requests.delete(
+        base_url + endpoint["table"] + "?db_name=" + database,
+        json={"table_name": table},
+    )
     assert response.status_code == 202, f"Unexpected status code: {response.status_code}"
 
 def test_delete_database(base_url, endpoint, database):
-    response = requests.delete(base_url + endpoint["db"] , json={"db_name": database})
+    response = requests.delete(
+        base_url + endpoint["db"],
+        json={"db_name": database},
+    )
     assert response.status_code == 202, f"Unexpected status code: {response.status_code}"
